abf207d05451f42dad321626bf2c9d4887d7da01
Rishabh <rarishabhjain@gmail.com>
2025-09-07 22:45:16 +0530

    40:50

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,7 @@
-import { Image, Text, View } from "react-native";
-import { styles } from "../styles/auth.styles";
+import { Redirect } from 'expo-router'
 
-export default function Index() {
+export default function index() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello</Text>
-      <Image
-        source={{uri: "https://images.unsplash.com/photo-1756990637536-714b76296a30?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1NXx8fGVufDB8fHx8fA%3D%3D"}}
-        style={{
-          width: 200,
-          height: 200
-        }}
-      />
-    </View>
-  );
+    <Redirect href="/(tabs)" />
+  )
 }
